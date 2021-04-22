8d9acc22e33e63e724fc2cb808adda94f0a198f1
Fikayo <fikfikky@gmail.com>
2021-04-22 14:37:53 +0100

    Further increased timeout

--- users.test.js
+++ users.test.js
@@ -12,7 +12,7 @@ describe("GenericContainer", () => {
     beforeAll(async () => {
         container = await new GenericContainer('mysql', '5.7')
         .withExposedPorts(3306)
-        .withStartupTimeout(120000)
+        .withStartupTimeout(1200000)
         .withEnv('MYSQL_ALLOW_EMPTY_PASSWORD', '1')
         .withEnv('MYSQL_DATABASE', 'testdb')
         .start();
