5916a5c46da2e01d1e7d5a66de7d0daf894e93a4
Fikayo <fikfikky@gmail.com>
2021-04-23 12:36:12 +0100

    Config Added

--- users.test.js
+++ users.test.js
@@ -1,7 +1,7 @@
 const { GenericContainer, Wait } = require("testcontainers");
 //const mysql = require('mysql');
 const { createPool } = require('mysql2/promise')
-const { Client } = require('pg')
+const faker = require('faker');
 
 jest.setTimeout(3000000);
 
@@ -21,30 +21,59 @@ describe("GenericContainer", () => {
     }); */
 
     beforeEach(async () => {
+
+        let createTableSQL = "CREATE TABLE `users` ( `id` INT(2) NOT NULL AUTO_INCREMENT , `name` VARCHAR(100) NOT NULL , `email` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;"
         
         connection = await createPool({ 
             host: "mysql-28850-0.cloudclusters.net", 
             user: 'testuser', password: 'password', 
-            port: 28850
+            port: 28850,
+            database: 'mytestdb'
         })
         console.log('Connected to database');
+
+        await connection.query(createTableSQL);
     })
 
     it("works", async () => {
         try {
+
+            const total_test_users = 3;
+            let insertQueries=[];
+
+            for (let i = 0; i < total_test_users; i++) {
+
+                let insertSQL = `INSERT INTO users (id, name, email) VALUES (NULL, '${faker.name.findName()}', '${faker.internet.email()}');`;
+
+                insertQueries.push(connection.query(insertSQL))
+                
+            }
+
+            await Promise.all(insertQueries);
             
-            console.log(await (connection.query('SELECT count(*) FROM information_schema.columns')))
+            
+            //console.log(await (connection.query('SELECT count(*) FROM information_schema.columns')));
+            //await connection.query(insertSQL);
+            const [rows, fields] = await connection.query('SELECT * FROM users');
+            console.log(rows);
+
+            expect(rows.length).toBe(total_test_users);
 
         } catch (error) {
             console.log(error);
+            let dropTableSQL = "DROP TABLE IF EXISTS `users`";
+            await connection.query(dropTableSQL);
+            await connection.end();
         } 
 
-        expect('A B C').toContain('A');
+        
         
     }, 60000);
 
-   afterEach(() => {
-     connection.end();
+   afterEach(async () => {
+     let dropTableSQL = "DROP TABLE IF EXISTS `users`";
+     await connection.query(dropTableSQL);
+     await connection.end();
    })
 
    /* afterAll(async () => {
